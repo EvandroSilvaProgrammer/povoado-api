58c48bf69ee8e193bddd6b395320a31c6a8f4801
Evandro Silva AKA SkeBug <evandrosilva,programmer@gmail.com>
2022-11-03 11:27:16 +0100

    add contact routes

--- start/routes.ts
+++ start/routes.ts
@@ -4,4 +4,11 @@ Route.group(() => {
 
   Route.resource('/person', 'PersonController').apiOnly()
 
+  Route.get('/contact/', 'ContactsController.index')
+  Route.post('/contact/:id', 'ContactsController.store')
+  Route.get('/contact/name/:name', 'ContactsController.showContactByName');
+  Route.get('/contact/phone/:telefone', 'ContactsController.showContactByPhone');
+  Route.get('/contact/email/:email', 'ContactsController.showContactByEmail');
+  Route.put('/contact/:id', 'ContactsController.update');
+
 }).prefix('/api')
