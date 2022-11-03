1804c70e1a3735353d9220d6a9596b37e51f3f04
Evandro Silva AKA SkeBug <evandrosilva,programmer@gmail.com>
2022-11-03 12:17:40 +0100

    change param /contact/phone/:phone

--- start/routes.ts
+++ start/routes.ts
@@ -7,7 +7,7 @@ Route.group(() => {
   Route.get('/contact/', 'ContactsController.index')
   Route.post('/contact/:id', 'ContactsController.store')
   Route.get('/contact/name/:name', 'ContactsController.showContactByName');
-  Route.get('/contact/phone/:telefone', 'ContactsController.showContactByPhone');
+  Route.get('/contact/phone/:phone', 'ContactsController.showContactByPhone');
   Route.get('/contact/email/:email', 'ContactsController.showContactByEmail');
   Route.put('/contact/:id', 'ContactsController.update');
 
