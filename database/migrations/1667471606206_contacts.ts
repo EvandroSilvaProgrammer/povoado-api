9a4363c74e7d177c8ca6b10e1202e588320883f3
Evandro Silva AKA SkeBug <evandrosilva,programmer@gmail.com>
2022-11-03 12:00:21 +0100

    change references for FK

--- database/migrations/1667471606206_contacts.ts
+++ database/migrations/1667471606206_contacts.ts
@@ -11,7 +11,7 @@ export default class extends BaseSchema {
       table.string('adress') 
       table.integer('type').defaultTo(1) 
       table.boolean('status').defaultTo(true)
-      table.integer('person_id').unsigned().references('person.id').onDelete('CASCADE')
+      table.integer('person_id').unsigned().references('people.id').onDelete('CASCADE')
       // table.primary(['people_id', 'telefone', 'email', 'type'])
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
