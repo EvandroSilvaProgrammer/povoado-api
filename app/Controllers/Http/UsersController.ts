8348a12dd3cb39a07eb8f8a155f4016d85f076ac
Evandro Silva AKA SkeBug <evandrosilva,programmer@gmail.com>
2022-11-04 13:14:34 +0100

    create register function

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,5 +1,19 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import User from 'App/Models/User';
 
 export default class UsersController {
-    
+    public async register({ request, response }: HttpContextContract) {
+
+        const body = request.body()
+
+        const user = await User.create(body)
+
+        response.status(201)
+
+        return {
+            message: 'User created successfully',
+            data: user,
+        }
+    }
 }
